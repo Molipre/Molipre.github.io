88e519cc43550646a1ff8f9e853fa5faa296ff02
Molipre <xuyangdejing@163.com>
2020-10-14 17:57:18 +0800

    Site updated: 2020-10-14 17:57:18

--- js/myjs.js
+++ js/myjs.js
@@ -92,3 +92,13 @@ document.addEventListener('visibilitychange', function () {
 // if (ip_content != null && typeof (returnCitySN) != undefined) {
 //     ip_content.innerHTML = '欢迎来自 <span class="p red">' + returnCitySN["cname"] + "</span> 的小伙伴<br>" + "访问IP为： <span class='p cyan'>" + returnCitySN["cip"] + "</span><br>浏览器版本：<span class='p blue'>" + getBrowserInfo() + '</span>';
 // }
+
+// 添加图片top
+// var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
+// // 添加到返回顶部按钮下
+// document.getElementById("go-up").innerHTML += top_up;
+
+//footer心跳
+$(document).ready(function(e) {
+    $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heart"></i>  一川');
+});
