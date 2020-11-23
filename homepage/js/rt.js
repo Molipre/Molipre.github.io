a2c8b48d0c8ce13fe3cf57fba62ac47d4b26e5d3
Molipre <xuyangdejing@163.com>
2020-11-23 17:18:01 +0800

    Site updated: 2020-11-23 17:18:01

---
+++ homepage/js/rt.js
@@ -0,0 +1,28 @@
+  setInterval(() => {
+      let create_time = Math.round(new Date('2020-11-14 12:00:00').getTime() / 1000);
+      let timestamp = Math.round((new Date().getTime() + 8 * 60 * 60 * 1000) / 1000);
+      let second = timestamp - create_time;
+      let time = new Array(0, 0, 0, 0, 0);
+      // if (second >= 365 * 24 * 3600) {
+      //   time[0] = parseInt(second / (365 * 24 * 3600));
+      //   second %= 365 * 24 * 3600;
+      // }
+      if (second >= 24 * 3600) {
+        time[1] = parseInt(second / (24 * 3600));
+        second %= 24 * 3600;
+      }
+      if (second >= 3600) {
+        time[2] = parseInt(second / 3600);
+        second %= 3600;
+      }
+      if (second >= 60) {
+        time[3] = parseInt(second / 60);
+        second %= 60;
+      }
+      if (second > 0) {
+        time[4] = second;
+      }
+      currentTimeHtml = '一川的主页运行了' + ' ' + time[1] + ' 天 ' + time[2] + ' 时 ' + time[3] + ' 分 ' + time[4] + ' 秒啦 ';
+      //currentTimeHtml = '一川的奇思怪想屋运行了' + ' ' + time[0] + ' 年 ' + time[1] + ' 天 ' + time[2] + ' 时 ' + time[3] + ' 分 ' + time[4] + ' 秒';
+      document.getElementById("running-time").innerHTML = currentTimeHtml;
+    }, 1000);
